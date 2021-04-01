46256c541f04efa624e9038cb64b674bfc958a2e
Tal <talrasha007@gmail.com>
2021-04-01 16:39:42 +0800

    router-view

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,18 @@
 <template>
-  <img src="./assets/logo.png">
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
-  </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <el-menu
+    mode="horizontal"
+    :default-active="$route.matched.length > 0 && $route.matched[0].path"
+    :router=true
+  >
+    <el-menu-item index="/">首页</el-menu-item>
+    <el-menu-item index="/about">关于</el-menu-item>
+  </el-menu>
+  <router-view />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  margin: 0;
+  padding: 0;
 }
 </style>
