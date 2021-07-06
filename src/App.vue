c29600fa598e508994bb938d8d7dc9b32ea7866d
Tal <talrasha007@gmail.com>
2021-07-06 18:34:55 +0800

    nav

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <template>
 <n-config-provider :theme="darkTheme">
+  <n-layout-header bordered>
+    <n-menu
+      mode="horizontal"
+      :value="route.path"
+      :options="navItems"
+      :render-label="renderNavItem"
+    />
+  </n-layout-header>
   <router-view />
   <n-global-style />
 </n-config-provider>
 </template>
 
 <script setup>
-import { NGlobalStyle, NConfigProvider, darkTheme } from 'naive-ui'
+import { h } from 'vue'
+import { RouterLink, useRoute } from 'vue-router'
+import { NConfigProvider, NText, NLayoutHeader, NMenu, NGlobalStyle, darkTheme } from 'naive-ui'
+
+const route = useRoute()
+
+const navItems = [{
+  key: '/',
+  label: '首页'
+},{
+  key: '/settings',
+  label: '设置'
+}]
+
+const renderNavItem = (option) => h(
+  RouterLink,
+  { to: option.key },
+  { default: () => option.label }
+)
 </script>
