31631d0c3fcce0720bd1c444edcfbdaed17feff0
Tal <talrasha007@gmail.com>
2021-07-17 21:20:53 +0800

    table scroll

---
+++ src/components/AccountTable.vue
@@ -0,0 +1,76 @@
+<template>
+  <n-data-table :columns="accountsColumns" :summary="accountsSummary" :data="accounts" :row-class-name="getRowClassName" :scroll-x="getScrollX()" />
+</template>
+
+<script setup>
+import { h } from 'vue'
+import { NDataTable } from 'naive-ui'
+import { accounts } from '@/store/okex'
+
+function getScrollX() {
+  return window.innerWidth < 720 ? 720 : ''
+}
+
+function getRowClassName(row) {
+  return row.upl > 0 ? 'red' : row.upl < 0 ? 'green' : ''
+}
+
+const accountsColumns = [{
+  title: 'CCY',
+  key: 'ccy',
+  fixed: 'left'
+}, {
+  title: '权益',
+  key: 'eq',
+  align: 'right',
+  render(row) {
+    return h('span', (row.eq * 1).toFixed(4))
+  }
+}, {
+  title: '收益',
+  key: 'upl',
+  align: 'right',
+  className: 'upl',
+  render(row) {
+    return h('span', (row.upl * 1).toFixed(4))
+  }
+}, {
+  title: '可用',
+  key: 'availEq',
+  align: 'right',
+  render(row) {
+    return h('span', (row.availEq * 1).toFixed(4))
+  }
+}, {
+  title: '使用率',
+  key: 'availEq',
+  align: 'right',
+  render(row) {
+    return h('span', ((1 - row.availEq / row.eq) * 100).toFixed(2) + '%')
+  }
+}, {
+  title: '$',
+  key: 'eqUsd',
+  align: 'right',
+  render(row) {
+    return h('span', (row.eqUsd * 1).toFixed(4))
+  }
+}]
+
+function accountsSummary(accounts) {
+  return {
+    ccy: { value: '总计' },
+    eq: { value: accounts.reduce((t, acc) => t + acc.eqUsd * 1 , 0).toFixed(4), colSpan: 5 }
+  }
+}
+</script>
+
+<style scoped>
+::v-deep(.red .upl) {
+  color: red;
+}
+
+::v-deep(.green .upl) {
+  color: green;
+}
+</style>
---
+++ src/components/PositionTable.vue
@@ -0,0 +1,81 @@
+<template>
+  <n-data-table v-for="g in groupedPositions" :key="g.gropId" :row-class-name="getRowClassName" :columns="getPositionColumns(g)" :data="g.positions" :scroll-x="getScrollX()" />
+</template>
+
+<script setup>
+import { h, computed } from 'vue'
+import { NDataTable } from 'naive-ui'
+import { positions } from '@/store/okex'
+
+function getScrollX() {
+  return window.innerWidth < 720 ? 720 : ''
+}
+
+function getRowClassName(row) {
+  return row.upl > 0 ? 'red' : row.upl < 0 ? 'green' : ''
+}
+
+const groupedPositions = computed(() => positions.value.reduce((g, p) => {
+  const groupId = /[A-Z]+-[A-Z]+/g.exec(p.instId)[0]
+  let group = g.find(v => v.groupId === groupId)
+  if (!group) {
+    g.push({ groupId, positions: [p] })
+  } else {
+    group.positions.push(p)
+  }
+
+  return g
+}, []))
+
+function getPositionColumns({ groupId }) {
+  return [{
+    title: groupId,
+    key: 'instId',
+    fixed: 'left'
+  }, {
+    title: '方向',
+    key: 'posSide'
+  }, {
+    title: '持仓量',
+    key: 'pos',
+    align: 'right'
+  }, {
+    title: '持仓量(A)',
+    key: 'availPos',
+    align: 'right'
+  }, {
+    title: '杠杆',
+    key: 'lever',
+    align: 'right'
+  }, {
+    title: '开仓均价',
+    key: 'avgPx',
+    align: 'right',
+    render(row) {
+      return h('span', (row.avgPx * 1).toFixed(4))
+    }
+  }, {
+    title: '盈亏',
+    key: 'upl',
+    align: 'right',
+    className: 'upl',
+    render(row) {
+      return h('span', (row.upl * 1).toFixed(4))
+    }
+  }, {
+    title: '强平价',
+    key: 'liqPx',
+    align: 'right'
+  }]
+}
+</script>
+
+<style scoped>
+::v-deep(.red .upl) {
+  color: red;
+}
+
+::v-deep(.green .upl) {
+  color: green;
+}
+</style>
