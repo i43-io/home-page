b06537046f2cc844455870c77f3a51f2bbb382af
Tal <talrasha007@gmail.com>
2021-07-17 19:32:10 +0800

    positions

--- src/store/okex.js
+++ src/store/okex.js
@@ -7,5 +7,17 @@ const { apiKey, apiSecret, passphrase } = config.value.okex
 const httpApi = new v5.HttpApi(apiKey, apiSecret, passphrase, { baseURL: 'https://ph8xwdmyl4.execute-api.us-east-1.amazonaws.com/prod' })
 
 export const accounts = ref([])
-const coins = ['ETH', 'BTC', 'USDT']
-httpApi.getAccounts(coins).then(({ details }) => accounts.value = details)
+export const positions = ref([])
+
+export async function updateAccount() {
+  const coins = ['ETH', 'BTC', 'USDT']
+  const { details } = await httpApi.getAccounts(coins)
+  accounts.value = details
+
+  positions.value = [
+    ...await httpApi.getPositions('FUTURES'),
+    ...await httpApi.getPositions('SWAP')
+  ]
+}
+
+updateAccount()
--- src/views/Positions.vue
+++ src/views/Positions.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="body-container">
-    <n-card title="资金">
-      <n-data-table :columns="accountsColumns" :summary="accountsSummary" :data="accounts" scroll-x></n-data-table>
+    <n-card title="资金" style="margin-bottom: 10px;">
+      <n-data-table :columns="accountsColumns" :summary="accountsSummary" :data="accounts" :row-class-name="getRowClassName" />
     </n-card>
-    <br/>
-    <n-card title="持仓">
-
+    <n-card title="持仓" >
+      <n-data-table v-for="g in groupedPositions" :key="g.gropId" :row-class-name="getRowClassName" :columns="getPositionColumns(g)" :data="g.positions" />
     </n-card>
   </div>
 </template>
 
 <script setup>
-import { h } from 'vue'
+import { h, computed } from 'vue'
 import { NCard, NDataTable } from 'naive-ui'
 
-import { accounts } from '@/store/okex'
+import { accounts, positions} from '@/store/okex'
+
+function getRowClassName(row) {
+  return row.upl > 0 ? 'red' : row.upl < 0 ? 'green' : ''
+}
 
 const accountsColumns = [{
   title: 'CCY',
@@ -23,27 +26,36 @@ const accountsColumns = [{
 }, {
   title: '权益',
   key: 'eq',
+  align: 'right',
   render(row) {
     return h('span', (row.eq * 1).toFixed(4))
   }
 }, {
   title: '收益',
   key: 'upl',
+  align: 'right',
+  className: 'upl',
   render(row) {
     return h('span', (row.upl * 1).toFixed(4))
   }
 }, {
   title: '可用',
-  key: 'availEq'
+  key: 'availEq',
+  align: 'right',
+  render(row) {
+    return h('span', (row.availEq * 1).toFixed(4))
+  }
 }, {
   title: '使用率',
   key: 'availEq',
+  align: 'right',
   render(row) {
     return h('span', ((1 - row.availEq / row.eq) * 100).toFixed(2) + '%')
   }
 }, {
   title: '$',
   key: 'eqUsd',
+  align: 'right',
   render(row) {
     return h('span', (row.eqUsd * 1).toFixed(4))
   }
@@ -55,4 +67,68 @@ function accountsSummary(accounts) {
     eqUsd: { value: accounts.reduce((t, acc) => t + acc.eqUsd * 1 , 0).toFixed(4) }
   }
 }
+
+const groupedPositions = computed(() => positions.value.reduce((g, p) => {
+  const groupId = /[A-Z]+-[A-Z]+/g.exec(p.instId)[0]
+  let group = g.find(v => v.groupId === groupId)
+  if (!group) {
+    g.push({ groupId, positions: [p] })
+  } else {
+    group.positions.push(p)
+  }
+
+  return g
+}, []))
+
+function getPositionColumns({ groupId }) {
+  return [{
+    title: groupId,
+    key: 'instId',
+    fixed: 'left'
+  }, {
+    title: '方向',
+    key: 'posSide'
+  }, {
+    title: '持仓量',
+    key: 'pos',
+    align: 'right'
+  }, {
+    title: '持仓量(A)',
+    key: 'availPos',
+    align: 'right'
+  }, {
+    title: '杠杆',
+    key: 'lever',
+    align: 'right'
+  }, {
+    title: '开仓均价',
+    key: 'avgPx',
+    align: 'right',
+    render(row) {
+      return h('span', (row.avgPx * 1).toFixed(4))
+    }
+  }, {
+    title: '盈亏',
+    key: 'upl',
+    align: 'right',
+    className: 'upl',
+    render(row) {
+      return h('span', (row.upl * 1).toFixed(4))
+    }
+  }, {
+    title: '强平价',
+    key: 'liqPx',
+    align: 'right'
+  }]
+}
 </script>
+
+<style scoped>
+::v-deep(.red .upl) {
+  color: red;
+}
+
+::v-deep(.green .upl) {
+  color: green;
+}
+</style>
