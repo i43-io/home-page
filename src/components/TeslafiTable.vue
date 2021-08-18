fb30e5d9b17c5183550f8176bdba60aee1007079
Tal <talrasha007@gmail.com>
2021-08-18 12:07:07 +0800

    release notes link

--- src/components/TeslafiTable.vue
+++ src/components/TeslafiTable.vue
@@ -8,15 +8,42 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
-import { NDataTable } from 'naive-ui'
+import { computed, h } from 'vue'
+import { NDataTable, useThemeVars } from 'naive-ui'
 
 import { teslafi } from '@/store/teslafi'
+
+const themeVars = useThemeVars()
+themeVars.value // weired
 
 function getScrollX() {
   return window.innerWidth < 720 ? 720 : ''
 }
 
-const columns = computed(() => teslafi.value && teslafi.value.latestUpdates &&
-  teslafi.value.latestUpdates.head.map((title, key) => ({ title, key, fixed: key === 0 ? 'left' : undefined, align: 'center' })))
+const columns = computed(() =>
+  teslafi.value && teslafi.value.latestUpdates &&
+  teslafi.value.latestUpdates.head.map((title, key) =>
+    key !== 0 ?
+    { title, key, align: 'center' } :
+    {
+      title, key, fixed: 'left', align: 'center',
+      render(row) {
+        return h(
+          'A',
+          {
+            target: '_blank',
+            href: `https://www.notateslaapp.com/software-updates/version/2021.24.3/release-notes`
+          },
+          { default: () => row[0] }
+        )
+      }
+    }
+  )
+)
 </script>
+
+<style scoped>
+::v-deep a {
+  color: v-bind(themeVars.primaryColor);
+}
+</style>
